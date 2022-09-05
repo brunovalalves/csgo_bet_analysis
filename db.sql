2357ec2505114920d419e874897334b010160baa
Bruno <bruno.valalves@gmail.com>
2022-09-05 19:27:11 -0300

    inserção na bd

--- db.sql
+++ db.sql
@@ -3,3 +3,21 @@ create table matches (id int not null primary key, game varchar(255) not null , 
 alter table matches add column winner int null;
 
 alter table matches add constraint ck_winner check (winner in (1,2));
+
+select sum(odd_resultado) from (select *, 
+  case 
+   when odd_team2 > odd_team1 and winner = 2 then odd_team2 
+   when odd_team1 > odd_team2 and winner = 1 then odd_team1
+   else 0
+  end as odd_resultado
+   from matches
+ where winner is not null) as underdog_winner;
+ 
+select sum(odd_resultado) from (select *, 
+  case 
+   when odd_team2 < odd_team1 and winner = 2 then odd_team2 
+   when odd_team1 < odd_team2 and winner = 1 then odd_team1
+   else 0
+  end as odd_resultado
+   from matches
+ where winner is not null) as favorite_winner;
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from gettext import find
+import time
 import psycopg2
 import requests
 from bs4 import BeautifulSoup
@@ -29,9 +29,11 @@ def select_matches_without_winners(conn):
     cursor = conn.cursor()
     cursor.execute('select id, game from matches where winner is null')
     return cursor.fetchall()
-    
-    
- 
+
+def write_winner(conn, id, win):
+    cursor = conn.cursor()
+    cursor.execute('update matches set winner = ' + str(win) + ' where id = ' + str(id))       
+    conn.commit()
 
 def get_odds(id, game):
     bets_request = requests.get ("https://www.hltv.org/betting/analytics/"+id+"/"+game)    
@@ -59,43 +61,40 @@ def main():
     )
     #Setting auto commit false
     conn.autocommit = True
-    # matches_request = requests.get ("https://www.hltv.org/matches")    
-    # matches_request_bs = BeautifulSoup(matches_request.content, "html.parser") 
-    # for a in matches_request_bs.find_all('a',{'class':'match'}):
-    #     id = a['href'].split('/')[2]        
-    #     print('Analisando o jogo '+str(id))
-    #     game = a['href'].split('/')[3]
-    #     odds = get_odds(id, game)       
-    #     if (odds[0]==0 or odds[1]==0 or select_match(conn, id) != None):
-    #         print('NOT inserted on DB')
-    #     else:  
-    #         print('inserted on DB')       
-    #         insert_match(conn, id, game, odds[0], odds[1])
+    matches_request = requests.get ("https://www.hltv.org/matches")    
+    matches_request_bs = BeautifulSoup(matches_request.content, "html.parser") 
+    for a in matches_request_bs.find_all('a',{'class':'match'}):
+        id = a['href'].split('/')[2]        
+        print('Analisando o jogo '+str(id))
+        game = a['href'].split('/')[3]
+        odds = get_odds(id, game)       
+        if (odds[0]==0 or odds[1]==0 or select_match(conn, id) != None):
+            print('NOT inserted on DB')
+        else:  
+            print('inserted on DB')       
+            insert_match(conn, id, game, odds[0], odds[1])
 
     for match in select_matches_without_winners(conn):
         results_request = requests.get ("https://www.hltv.org/matches/"+str(match[0])+"/"+match[1])    
         results_request_bs = BeautifulSoup(results_request.content, "html.parser")
-        div = results_request_bs.find('div',{'class':'won'})        
+        div = results_request_bs.find('div',{'class':'won'})
+        win = 0 
+        time.sleep (1)
         if div != None:
             if 'team1' in div.parent['class'][0]:
+                win = 1
                 print('Time 1 vencedor')
+                write_winner (conn, match[0], win)                
             elif 'team2' in div.parent['class'][0]:
                 print('Time 2 vencedor')
+                win = 2
+                write_winner (conn, match[0], win)                          
             else:
                 print('Nenhum time vencedor')
         else:
             print('Jogo ainda não acabou')
-
-
-    # for a in results_request_bs.find_all('a',{'class':'a-reset'}):
-    #     id = a['href'].split('/')[2]
-    #     print('Verificando resultado do jogo '+str(id))
-    #     game = a['href'].split('/')[3]
-    #     for r in results_request_bs.find_all('div',{'class':'team team won'}):
-    #         winner = r['team team won']
-    #         print(winner)
-    #         #results = get_results(id,game)
-        
+            print (match, div, results_request)
+      
     # Closing the connection
     conn.close()        
     
